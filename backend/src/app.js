85585e50f4d2ec771c7f5b8af4acf287602ef05c
kumara291098 <kumara291098@gmail.com>
2025-07-30 17:54:46 +0530

    Add backend code

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+const express= require('express');
+const cors = require('cors');
+const taskRoutes = require('./routes/taskRoutes');
+
+const app=express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/tasks', taskRoutes);
+const PORT=process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
